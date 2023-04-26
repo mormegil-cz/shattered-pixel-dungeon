1a3974a3c87bfb2e5959d7dd0a56943276dc49da
Mormegil <mormegil@centrum.cz>
2023-04-26 17:15:38 +0200

    Customizations

--- core/src/main/java/com/shatteredpixel/shatteredpixeldungeon/actors/hero/Hero.java
+++ core/src/main/java/com/shatteredpixel/shatteredpixeldungeon/actors/hero/Hero.java
@@ -198,7 +198,7 @@ public class Hero extends Char {
 	
 	public static final int MAX_LEVEL = 30;
 
-	public static final int STARTING_STR = 10;
+	public static final int STARTING_STR = 18;
 	
 	private static final float TIME_TO_REST		    = 1f;
 	private static final float TIME_TO_SEARCH	    = 2f;
@@ -243,7 +243,7 @@ public class Hero extends Char {
 	public Hero() {
 		super();
 
-		HP = HT = 20;
+		HP = HT = 80;
 		STR = STARTING_STR;
 		
 		belongings = new Belongings( this );
@@ -729,7 +729,7 @@ public class Hero extends Char {
 		}
 
 		speed = AscensionChallenge.modifyHeroSpeed(speed);
-		
+
 		return speed;
 		
 	}
@@ -2188,11 +2188,30 @@ public class Hero extends Char {
 
 			}
 			return;
+		} else {
+
+			interrupt();
+			resting = false;
+
+			this.HP = HT * 2 / 3;
+			this.STR++;
+
+			PotionOfHealing.cure(this);
+			Buff.prolong(this, Invulnerability.class, Invulnerability.DURATION);
+
+			SpellSprite.show(this, SpellSprite.ANKH);
+			GameScene.flash(0x80FFFF40);
+			Sample.INSTANCE.play(Assets.Sounds.TELEPORT);
+			GLog.w(Messages.get(this, "revive"));
+			Statistics.ankhsUsed++;
+			Catalog.countUse(Ankh.class);
+
+			return;
 		}
 		
-		Actor.fixTime();
-		super.die( cause );
-		reallyDie( cause );
+//		Actor.fixTime();
+//		super.die( cause );
+//		reallyDie( cause );
 	}
 	
 	public static void reallyDie( Object cause ) {
@@ -2560,6 +2579,7 @@ public class Hero extends Char {
 		}
 		
 		if (intentional) {
+			new ScrollOfMagicMapping().collect();
 			sprite.showStatus( CharSprite.DEFAULT, Messages.get(this, "search") );
 			sprite.operate( pos );
 			if (!Dungeon.level.locked) {
